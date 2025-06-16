47cfe5f5e5c11f8d41dcff7b07c3a8c65fba76d4
Drew Williams <williams.r.drew@gmail.com>
2025-06-16 16:10:24 -0400

    formatting

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -3,18 +3,18 @@
 #include <iostream>
 
 int main() {
-    fmt::print("Hello from TagFinder!\n");
-    fmt::print("{}\n", apriltag::get_version());
-    
-    apriltag::Calculator calc;
-    
-    int a = 10;
-    int b = 5;
-    
-    fmt::print("{} + {} = {}\n", a, b, calc.add(a, b));
-    fmt::print("{} - {} = {}\n", a, b, calc.subtract(a, b));
-    fmt::print("{} * {} = {}\n", a, b, calc.multiply(a, b));
-    fmt::print("{} / {} = {:.2f}\n", a, b, calc.divide(a, b));
-    
-    return 0;
+  fmt::print("Hello from TagFinder!\n");
+  fmt::print("{}\n", apriltag::get_version());
+
+  apriltag::Calculator calc;
+
+  int a = 10;
+  int b = 5;
+
+  fmt::print("{} + {} = {}\n", a, b, calc.add(a, b));
+  fmt::print("{} - {} = {}\n", a, b, calc.subtract(a, b));
+  fmt::print("{} * {} = {}\n", a, b, calc.multiply(a, b));
+  fmt::print("{} / {} = {:.2f}\n", a, b, calc.divide(a, b));
+
+  return 0;
 }
--- src/apriltag/apriltag.cpp
+++ src/apriltag/apriltag.cpp
@@ -5,26 +5,26 @@
 namespace apriltag {
 
 int Calculator::add(int a, int b) {
-    return a + b;
+  return a + b;
 }
 
 int Calculator::subtract(int a, int b) {
-    return a - b;
+  return a - b;
 }
 
 int Calculator::multiply(int a, int b) {
-    return a * b;
+  return a * b;
 }
 
 double Calculator::divide(int a, int b) {
-    if (b == 0) {
-        throw std::invalid_argument("Division by zero");
-    }
-    return static_cast<double>(a) / static_cast<double>(b);
+  if (b == 0) {
+    throw std::invalid_argument("Division by zero");
+  }
+  return static_cast<double>(a) / static_cast<double>(b);
 }
 
 std::string get_version() {
-    return fmt::format("AprilTag version {}.{}.{}", 0, 0, 1);
+  return fmt::format("AprilTag version {}.{}.{}", 0, 0, 1);
 }
 
-} // namespace apriltag
+}  // namespace apriltag
