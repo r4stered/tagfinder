4de12ffc73bfca223b94d5745d3284f98e80a440
Drew Williams <williams.r.drew@gmail.com>
2025-06-16 13:45:58 -0400

    fixed test

--- src/apriltag/apriltag.cpp
+++ src/apriltag/apriltag.cpp
@@ -24,7 +24,7 @@ double Calculator::divide(int a, int b) {
 }
 
 std::string get_version() {
-    return fmt::format("Apriltag version {}.{}.{}", 1, 0, 0);
+    return fmt::format("AprilTag version {}.{}.{}", 0, 0, 1);
 }
 
 } // namespace apriltag
